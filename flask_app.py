d49095a22ef9df9868a2e3fff2576783b3a3b7ac
Ryan McGough <132298917+rhmcgough@users.noreply.github.com>
2025-11-29 22:14:30 -0800

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,28 +1,43 @@
+# flask_app.py
+# this is the API for discord bot.
+
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-# TODO: Replace this with calls to get the real data. 
+# Data structure for stats
 bot_stats = {
     "guilds": [],
+    "voice-channels": [],
+    "text-channels": [],
+    "roles": [],
     "users": 0,
     "status": "offline"
+
 }
 
-def update_bot_stats(guilds, user_count, status):
+
+# Updates data structure
+def update_bot_stats(guilds, user_count, status, voice_channels, text_channels, roles):
     bot_stats['guilds'] = guilds
     bot_stats['users'] = user_count
     bot_stats['status'] = status
+    bot_stats['voice-channels'] = voice_channels
+    bot_stats['text-channels'] = text_channels
+    bot_stats['roles'] = roles
+
 
 @app.route('/api/bot-stats', methods=['GET'])
 def get_bot_stats():    
     return jsonify(bot_stats)
 
+
 @app.route('/')
 def home():
         return render_template('home.html', guilds=get_guilds())
+
 
 def get_guilds():
     file = open('guilds.txt', 'r')
@@ -33,5 +48,6 @@ def get_guilds():
     guilds = data
     return guilds
 
+
 def run_flask():
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8005, debug=False)
